016d370968b5d03129a9486cddc314815eccdf35
w-mills <wmills@financeit.io>
2020-03-25 18:23:37 -0400

    add all content upto and including video #45

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,27 @@
 class ApplicationController < JSONAPI::ResourceController
-  def authenticate
-    begin
-      authenticate_or_request_with_http_token do |token, options|
-        verified_token = JWT.decode token, JWT_SECRET, true, { algorithm: "HS512" }
+  protect_from_forgery with: :null_session
 
-        user_id = verified_token[0]["sub"]
-
-        @current_user = User.find(user_id)
+  class ApplicationController < JSONAPI::ResourceController
+    def authenticate
+      begin
+        authenticate_or_request_with_http_token do |token, options|
+          verified_token = JWT.decode token, JWT_SECRET, true, { algorithm: "HS512" }
+  
+          user_id = verified_token[0]["sub"]
+  
+          @current_user = User.find(user_id)
+        end
+      rescue
+        render status: :unauthorized, json: {
+          errors: [
+            {
+              status: 401,
+              title: "Unauthorized"
+            }
+          ]
+        }
       end
-    rescue
-      render status: :unauthorized, json: {
-        errors: [
-          {
-            status: 401,
-            title: "Unauthorized"
-          }
-        ]
-      }
     end
   end
+  
 end
