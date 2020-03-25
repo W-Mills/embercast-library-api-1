1ee1596d98ccfc9ad54b31fedcfc2445e17601e4
w-mills <wmills@financeit.io>
2020-03-25 15:53:55 -0400

    all content upto and including video #42

--- Gemfile
+++ Gemfile
@@ -25,10 +25,11 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+gem 'jwt'
 
+gem 'jsonapi-resources', '0.9.11'
 gem 'rack-cors'
-gem 'jsonapi-resources'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_212243) do
+ActiveRecord::Schema.define(version: 2020_03_25_183110) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2020_03_24_212243) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
